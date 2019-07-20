f3314d465cd00510cd301a123d7f80b36d4c22e2
SiRyuA <SiRyuA@users.noreply.github.com>
2019-07-21 00:50:05 +0900

    20190721 0050

--- app/textcounter/app.js
+++ app/textcounter/app.js
@@ -1,7 +1,9 @@
 let ov;
 
 $("#text").on("propertychange change keyup paste input", function() {
-    let cv = $(this).val().replace(/[^(가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9)]/gi, "");
+    let cv = $(this).val();
+	cv = cv.replace(/[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi, "")
+		 .replace(/[^(가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9)]/gi, "");
     if(cv == ov) return;
     else ov = cv;
     document.getElementById("count").innerText = cv.length;
