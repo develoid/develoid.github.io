9deee0f644ebaaf8b0a75fc5a15759ca461a8358
SiRyuA <SiRyuA@users.noreply.github.com>
2019-07-29 10:39:35 +0900

    20190729 1039

--- app/vatcal/app.js
+++ app/vatcal/app.js
@@ -3,8 +3,8 @@ $("#i10").on("propertychange change keyup paste input", function() {
 	let price = (source/1.1).toFixed();
 	let vat = source-price;
 	
-    document.getElementById("o11").value = price;
-    document.getElementById("o12").value = vat;
+    document.getElementById("o11").value = numberWithCommas(price);
+    document.getElementById("o12").value = numberWithCommas(vat);
 });
 
 $("#i20").on("propertychange change keyup paste input", function() {
@@ -12,6 +12,10 @@ $("#i20").on("propertychange change keyup paste input", function() {
 	let vat = (source*0.1).toFixed();
 	let price = parseInt(source)+parseInt(vat);
 	
-    document.getElementById("o21").value = price;
-    document.getElementById("o22").value = vat;
+    document.getElementById("o21").value = numberWithCommas(price);
+    document.getElementById("o22").value = numberWithCommas(vat);
 });
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
