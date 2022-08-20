265ee15a6241179013136879e8df4ef69c23724c
heart <66947260+bampudding@users.noreply.github.com>
2022-08-20 23:55:11 +0900

    Timer

---
+++ asset/js/timer.js
@@ -0,0 +1,12 @@
+var count = 15;
+var counter = setInterval(timer, 1000);
+function timer() {
+  count--;
+  if (count <= 0) {
+    clearInterval(counter);
+    document.getElementById("timer").innerHTML = "0";
+    window.location.replace('링크')
+    return;
+  }
+  document.getElementById("timer").innerHTML = count;
+}
