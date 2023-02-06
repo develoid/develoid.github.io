02978282f29d235e2c9a505f40691d3e1219b641
heart <66947260+bampudding@users.noreply.github.com>
2023-02-06 20:25:33 +0900

    HeartService Test

--- heart-dvlservice/asset/js/color.js
+++
@@ -1,2 +0,0 @@
-let div = document.getElementsByClassName('.Support');
-div.classList.add('sidebar-on');
--- heart-dvlservice/asset/js/header-scroll.js
+++ heart-dvlservice/asset/js/header-scroll.js
@@ -1,9 +1,12 @@
 document.addEventListener('scroll', function(){
     let scroll = document.body.scrollTop
-    if (scroll > 16) {
+    if (scroll > 48) {
         document.querySelector('.PC-Header').classList.add('Fix');
+        document.querySelector('.PC-Header_dummy').classList.remove('display-none');
     }
+
     else {
         document.querySelector('.PC-Header').classList.remove('Fix');
+        document.querySelector('.PC-Header_dummy').classList.add('display-none');
     } 
 });
